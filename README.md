9fced76df42edd0ec9700fe82683c2767e46f777
conscou <scoullisconstantina@gmail.com>
2017-01-15 02:59:53 -0500

    included branding - still needs work

---
+++ README.md
@@ -0,0 +1,13 @@
+HackHERS
+
+Check out HackHERS(http://hackhers.us), an all women hackathon in Rutgers University, NJ.
+
+This project is forked from AlexEKoren/Technify.
+
+Setup
+
+pip install -r requirements.txt
+Running the app
+
+python app.py
+Runs on http://localhost:5000 by default.
---
+++ app.py
@@ -0,0 +1,67 @@
+from PIL import Image, ImageChops, ImageEnhance
+import requests
+import os
+from flask import Flask, redirect, jsonify, render_template, request, send_file
+import werkzeug
+import datetime
+import uuid
+
+app = Flask(__name__)
+
+ALLOWED_IMAGE_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+def process_image(img):
+  #open up the mask
+  mask = Image.open('mask.png')
+  mask = mask.convert('RGBA')
+  #make sure it matches the size of the image
+  mask = mask.resize(img.size)
+
+  #make sure our image has alpha channel
+  img = img.convert('RGBA')
+
+  #unique name
+  filename = uuid.uuid4().hex + '.png'
+  filename = os.path.join('/tmp', filename)
+  Image.alpha_composite(img, mask).save(filename, 'PNG')
+  #send it back
+  return filename
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/hackhers-2017-filter', methods=['POST'])
+def classify_upload():
+  try:
+    #get the image from the request
+    imagefile = request.files['imagefile']
+    filename_ = str(datetime.datetime.now()).replace(' ', '_') + \
+            werkzeug.secure_filename(imagefile.filename)
+    filename = os.path.join('/tmp', filename_)
+
+    #make sure it has the correct file type
+    b = False
+    for ext in ALLOWED_IMAGE_EXTENSIONS:
+      if ext in filename:
+        b = True
+    if not b:
+      return 'Invalid filetype.'
+
+    #save the file to /tmp
+    imagefile.save(filename)
+    #open the image for Pillow
+    image = Image.open(filename)
+  except Exception as err:
+    #uh oh. Something went wrong.
+    print 'Uploaded image open error: ' + err
+    return 'Error: ' + err
+
+  #process the image
+  resultFilename = process_image(image)
+  #send it back
+  return send_file(resultFilename, mimetype='image/png', as_attachment=True, attachment_filename='hackhersify.png')
+
+if __name__ == '__main__':
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port)
